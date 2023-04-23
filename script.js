2233b55db2914413da7e46f3e42682ba08668cdb
mpcortezi <128532216+mpcortezi@users.noreply.github.com>
2023-04-23 16:39:23 -0300

    Game is working properly now. It works as a best of 5, whoever gets 3 wins, wins the game

--- script.js
+++ script.js
@@ -1,45 +1,63 @@
-function getUserChoice(userInput){
-    userInput = userInput.toLowerCase(); // convert to lowercase
-    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors'){
-        return userInput;
+let playerScore = 0;
+let computerScore = 0;
+let result = '';
+
+function getUserChoice() {
+    let userInput = prompt('Choose your weapon (rock, paper, or scissors):');
+    userInput = userInput.toLowerCase();
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+      return userInput;
     } else {
-        console.log("Please select rock, paper, or scissors.");
+      console.log('Please select rock, paper, or scissors.');
+      return getUserChoice(); // prompt again if invalid input entered
     }
-}
+  }
+  
 
 function getComputerChoice(){
-    let randomNumber = Math.floor(Math.random()*3);
-    switch (randomNumber){
-        case 0:
-            return 'rock';
-        case 1:
-            return 'paper';
-        case 2:
-            return 'scissors';
-    }
+  let randomNumber = Math.floor(Math.random()*3);  
+  switch (randomNumber){
+    case 0:
+        return "rock";
+    case 1:
+        return "paper";
+    case 2:
+        return "scissors";
+  }
 }
 
-function playRound(){
-    const playerSelection = getUserChoice(prompt('Choose your weapon'));
-    const computerSelection = getComputerChoice();
-    let result;
-
+function playRound(playerSelection, computerSelection){
+    if (!playerSelection) {
+        console.log('Please enter a valid choice.');
+        return;
+    }
+    
     if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
-        (playerSelection == 'paper' && computerSelection == 'rock') ||
-        (playerSelection == 'scissors' && computerSelection == 'paper')){
-        result = 'You win! ' + playerSelection + ' beats ' + computerSelection;
+    (playerSelection == 'paper' && computerSelection == 'rock') ||
+    (playerSelection == 'scissors' && computerSelection == 'paper')){
+            result = ('You win!' + playerSelection + ' beats ' + computerSelection);
+            playerScore++;
     }
     else if (playerSelection == computerSelection){
-        result = 'It\'s a tie. You both chose ' + playerSelection;
+        result = ('It\'s a tie. You both chose ' + playerSelection);
     }
     else{
-        result = 'You lose! ' + computerSelection + ' beats ' + playerSelection;
+        result = ('You lose!' + computerSelection + ' beats ' + playerSelection);
+        computerScore++;
     }
-
+    
     console.log(result);
+    
+    if (playerScore === 3 || computerScore === 3) {
+        console.log('Game over!');
+        console.log(`Player score: ${playerScore}`);
+        console.log(`Computer score: ${computerScore}`);
+        return;
+    }
+    
+    const nextPlayerSelection = getUserChoice();
+    const nextComputerSelection = getComputerChoice();
+    playRound(nextPlayerSelection, nextComputerSelection);
 }
 
-// call playRound five times to play a best of five game
-for (let i = 0; i < 5; i++){
-    playRound();
-}
+playRound(getUserChoice(), getComputerChoice());
