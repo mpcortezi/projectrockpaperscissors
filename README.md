aaa91c59e61b17c296ccc6e58172063c44f3151a
mpcortezi <128532216+mpcortezi@users.noreply.github.com>
2023-04-23 16:15:58 -0300

    Now the game is working better

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ User prompt to choose between Rock, Paper or Scissors
 Computer pick a random number from 0 to 2
 Change numbers to rock(0) paper(1) or scissors(2)
 compare userchoice to computerchoice to see who wins or if it is a tie
-Make the game a best of 9 = who wins 5 games first is the champion
+Make the game a best of 5 = who wins 3 games first is the champion
--- script.js
+++ script.js
@@ -1,42 +1,45 @@
-function getComputerChoice(){
-  let randomNumber = Math.floor(Math.random()*3)  
-switch (randomNumber){
-    case 0:
-        return "rock";
-    case 1:
-        return "paper";
-    case 2:
-        return "scissors";
-}}
-
-let userInput = " ";
-const playerSelection = getUserChoice();
-const computerSelection = getComputerChoice();
-let result = "";
-
-
 function getUserChoice(userInput){
-    userInput = userInput.toLowerCase();
-      if (userInput === `rock`|| userInput === `paper` || userInput === `scissors`){
+    userInput = userInput.toLowerCase(); // convert to lowercase
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors'){
         return userInput;
-      } else {
-        console.log("Please select rock, paper or scissors")
+    } else {
+        console.log("Please select rock, paper, or scissors.");
+    }
+}
+
+function getComputerChoice(){
+    let randomNumber = Math.floor(Math.random()*3);
+    switch (randomNumber){
+        case 0:
+            return 'rock';
+        case 1:
+            return 'paper';
+        case 2:
+            return 'scissors';
+    }
+}
+
+function playRound(){
+    const playerSelection = getUserChoice(prompt('Choose your weapon'));
+    const computerSelection = getComputerChoice();
+    let result;
+
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'paper' && computerSelection == 'rock') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper')){
+        result = 'You win! ' + playerSelection + ' beats ' + computerSelection;
+    }
+    else if (playerSelection == computerSelection){
+        result = 'It\'s a tie. You both chose ' + playerSelection;
+    }
+    else{
+        result = 'You lose! ' + computerSelection + ' beats ' + playerSelection;
     }
 
+    console.log(result);
 }
 
-function playRound(playerSelection, computerSelection){
-    prompt("Choose one: Rock, Paper or Scissors?", userInput);
-    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
-    (playerSelection == 'paper' && computerSelection == 'rock') ||
-    (playerSelection == 'scissors' && computerSelection == 'paper')){
-    result = ('You win!' + playerSelection + 'beats' + computerSelection +)
-}
-else if (playerSelection == computerSelection){
-    result = ('It\'s a tie. You both chose' + playerSelection +)
-}
-else{
-    result = ('You lose!' + computerSelection + 'beats' + playerSelection +)
-}
-console.log(result)
+// call playRound five times to play a best of five game
+for (let i = 0; i < 5; i++){
+    playRound();
 }
